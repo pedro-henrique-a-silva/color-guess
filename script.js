61c1c818b5582831ebae8e95e12ec89da7f1a444
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-24 10:33:39 -0300

    Requisito 5 concluido, verificação do acerto

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const pRgbColor = document.querySelector('#rgb-color')
+const pRgbColor = document.querySelector('#rgb-color');
+const pAnswer = document.querySelector('#answer');
 const sectionColor = document.querySelector('#colors');
 
 const generateRandomNumber = (number) => Math.floor(Math.random() * (number));
@@ -14,12 +15,12 @@ const chooseColorGuess = () => {
   const colors = document.querySelectorAll('#colors .ball');
   const colorGuess = generateRandomNumber(colors.length);
   pRgbColor.innerHTML = colors[colorGuess].style.backgroundColor;
-}
+};
 
 const createDivColors = () => {
   for (let index = 0; index < 6; index += 1) {
     const divColor = document.createElement('div');
-    const randomColor = getRandomColor()
+    const randomColor = getRandomColor();
     divColor.classList.add('ball');
     divColor.style.backgroundColor = `rgb${randomColor}`;
     sectionColor.appendChild(divColor);
@@ -27,9 +28,26 @@ const createDivColors = () => {
   chooseColorGuess();
 };
 
+const guessVerify = (element) => {
+  const bgToGuess = pRgbColor.innerText;
+  const bgColorGuessed = element.style.backgroundColor;
 
+  if (bgToGuess === bgColorGuessed) {
+    pAnswer.innerText = 'Acertou!';
+  } else {
+    pAnswer.innerText = 'Errou! Tente novamente!';
+  }
+};
+
+const eventColor = () => {
+  sectionColor.addEventListener('click', (event) => {
+    if (event.target.classList.contains('ball')) {
+      guessVerify(event.target);
+    }
+  });
+};
 
 window.onload = () => {
   createDivColors();
-  
-}
+  eventColor();
+};
